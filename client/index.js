b79aeee85a98ed7e0d05f839318d4b6307f20b08
Isaac Pierce <contact@isaacdpierce.com>
2018-09-08 19:19:58 -0700

    Add Login and Auth forms

--- client/index.js
+++ client/index.js
@@ -5,6 +5,8 @@ import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
+
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -24,7 +26,7 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App} >
-
+          <Route path="login" component={LoginForm} />
         </Route>
       </Router>
     </ApolloProvider>
